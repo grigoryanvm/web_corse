14c5a2b2ed8930631ec34e31992b6b0053f95846
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-05 18:16:41 +0300

    Решила ту же задачу с помощью цикла for

--- lab1.php
+++ lab1.php
@@ -1,11 +1,23 @@
 <?php 
 $price=100;
 $price += $price * 0.1;
+echo "Цена продукта под влиянием инфляции спустя несколько лет:";
 echo $price;
 echo "</br>";
 while ($price<=150){
-   $price+=$price*0.35;
+   $price+=$price*0.035;
     echo $price;
     echo"</br>";
+}
+$price = 100;
+ 
+for($i = 0; $i < 6; $i++) {
+    if(!$i) {
+        $price += $price*0.1;
+        echo "В конце года:  ".$price.'руб. <br/>';
+    } else {
+        $price += $price*0.035;
+        echo "Через $i года(-а, лет):  ".$price.'руб. <br/>';
+    }
 }
 ?>
