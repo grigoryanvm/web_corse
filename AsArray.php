80d76085f78b142eac88bffb797242b2ac7eb9d9
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-05 19:37:05 +0300

    сортировка массива

--- AsArray.php
+++ AsArray.php
@@ -2,7 +2,11 @@
 $product = array("Чайник" => 500, "Сковородка" => 300, "Кастрюля" => 400);
 $product['Ложка'] = 50;
 $product['нож'] = 100;
+asort($product);
 foreach($product as $item => $item_count) {
     echo "Название продукта:" . $item . "  -Цена:" . $item_count.'<br/>';
+   
   }
+echo "Количество продуктов-".count($product)."<br/>";
+echo "Их общая сумма-".array_sum($product);
 ?>
