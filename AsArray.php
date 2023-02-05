d14bec84721bf4441e1bcd854a559f9703a350bf
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-05 19:07:50 +0300

    вывод на экран

--- AsArray.php
+++ AsArray.php
@@ -2,5 +2,7 @@
 $product = array("Чайник" => 500, "Сковородка" => 300, "Кастрюля" => 400);
 $product['Ложка'] = 50;
 $product['нож'] = 100;
-
+foreach($product as $item => $item_count) {
+    echo "Название продукта:" . $item . "  -Цена:" . $item_count.'<br/>';
+  }
 ?>
