8ed5d15436baf6243c584239ce0d959125a8d311
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-06 19:15:55 +0300

    заполнение массива случаными числами

--- seven.php
+++ seven.php
@@ -4,8 +4,15 @@ echo <<<HTML
 <p> Студентка 3 курса фспо специальности " Информационные ситестемы и программирование"<br>
     Начальные знания HTML,CSS,PHP.<p>
 HTML;
-$n = 100;
-while( )
-echo rand(0, 500);
+$n = 1000;
 
+$sum=0;
+$primer = array();
+//while(array_sum($primer)>$n){
+      for ($i = 0; $i < 10; $i++) {
+        $primer[$i] = rand(0, 100);
+        echo $primer[$i].'<br/>';
+        }
+  
+//}
 ?>
