9cc19a085b8c7d50021cfc5b61036e6cc7f905c1
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-08 20:42:07 +0300

    Добавила поля

--- form.php
+++ form.php
@@ -1,8 +1,10 @@
 <?php
 echo <<<HTML
 <form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Name: <input type="text" name="name"><br><br>
+Address: <input type="text" name="address"><br><br>
+E-mail: <input type="text" name="email"><br><br>
+Pasword: <input type="text" name="password"><br>
 <input type="submit">
 </form>
 HTML;
