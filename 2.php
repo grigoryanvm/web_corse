9318841475f5347262554785463addcca09cbb5b
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-08 20:34:24 +0300

    создание формы

--- 2.php
+++
@@ -1,2 +0,0 @@
-Welcome <?php echo $_GET["name"];  ?> from <?php echo $_GET["address"]?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+echo <<<HTML
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+HTML;
+?>
