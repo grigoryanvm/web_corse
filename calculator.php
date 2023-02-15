9fd905d4d750cd9700c9118ff5396172780bfa80
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-15 16:36:18 +0300

    вычислительные операции

--- calculator.php
+++ calculator.php
@@ -17,5 +17,26 @@ if(isset($_POST['submit'])){
         $number1 = $_POST['number1'];
         $number2 = $_POST['number2'];
         $operation = $_POST['operation'];}
+if(!$operation || (!$number1 && $number1 != '0') || (!$number2 && $number2 != '0')) {
+         $error_result = 'Не все поля заполнены';
+        }
 
+        switch($operation){ // проверяем какой оператор был выбран
+            case 'plus': // если выбран оператор сложения, то вычисляем сумму
+                $result = $number1 + $number2; 
+                break;
+            case 'minus': // если выбран оператор вычитания, то вычисляем разность
+                $result = $number1 - $number2;  
+                break;
+            case 'multiply': // если выбран оператор умножения, то вычисляем произведение
+                $result = $number1 * $number2;
+                break;
+            case 'divide': // если выбран оператор сложения
+                if( $number2 == '0') // проверям не является ли второй делитель нулевым
+                $error_result = "На ноль делить нельзя!"; 
+                else
+                    $result = $number1 / $number2; // если второй делитель не ноль, выполняем деление
+                    break;    
+            }
+    
 ?>
